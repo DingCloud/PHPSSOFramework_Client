3cfc9987bf25242d9ed5ee1b741cedc054191f4d
Jerry Cheung <SmallOyster@users.noreply.github.com>
2018-04-12 21:42:15 +0800

    use unified url config
    使用统一配置

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php
-$ssoLoginUrl = 'http://sso.local.dingstudio.cn/index.php?action=login&callbackUrl='.urlencode('http://app1.local.dingstudio.cn/login.php');
-if (!isset($_COOKIE['mysso_sign']) || !isset($_COOKIE['mysso_user'])) {
+include('config.php');
+$ssoLoginUrl=ssoUrl.'?action=login&callbackUrl='.urlencode(callbackUrl);
+
+if(!isset($_COOKIE['mysso_sign']) || !isset($_COOKIE['mysso_user'])){
     header('Location: '.$ssoLoginUrl);
     exit();
-}
-else {
-    setcookie('username', $_COOKIE['mysso_user'], time() + 3600, '/', $_SERVER['HTTP_HOST']);
-    setcookie('usersign', $_COOKIE['mysso_sign'], time() + 3600, '/', $_SERVER['HTTP_HOST']);
+}else{
+    setcookie('username',$_COOKIE['mysso_user'],time()+3600,'/',$_SERVER['HTTP_HOST']);
+    setcookie('usersign',$_COOKIE['mysso_sign'],time()+3600,'/',$_SERVER['HTTP_HOST']);
     header('Location: ./index.php');
 }
-
